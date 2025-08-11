722c1745e0ffc82ed9f7c56c83a129ca60c07100
lukasbelka <lukasbelka@gmail.com>
2025-08-11 19:51:06 +0200

    fonts and index adjusted

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ensaimble';
+export const SITE_DESCRIPTION = 'Data driven ai distilled decision making!';
